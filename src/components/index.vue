2e0f47aaa8b2d48a3983b15924cb3edd9b49d955
Zion0707 <Zion19920707@gmail.com>
2018-06-20 18:04:22 +0800

    change

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,15 +1,16 @@
 <template>
-    <div id="app">
-        <router-view/>
+    <div class="page">
+    	
     </div>
 </template>
 
 <script>
 export default {
-    data(){
+    data () {
         return {
 
         }
     }
 }
 </script>
+
