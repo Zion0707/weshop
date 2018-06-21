a51b1a9e682b00469d2c911267e9688248243c5d
Zion0707 <Zion19920707@gmail.com>
2018-06-21 22:22:30 +0800

    change

--- mobile/src/components/my/index.vue
+++ mobile/src/components/my/index.vue
@@ -14,6 +14,43 @@
                 </div>
             </div>
     	</div>
+        <yd-cell-group class="mb-not">
+            <yd-cell-item arrow>
+                <span slot="left">我的订单</span>
+                <span slot="right">全部订单</span>
+            </yd-cell-item>
+        </yd-cell-group>
+
+        <ul class="my-tool clearfix">
+           <li>
+               <i class="iconfont icon-fukuanfangshi"></i>
+               <span>代付款</span>
+           </li> 
+           <li>
+               <i class="iconfont icon-daishouhuo"></i>
+               <span>代收货</span>
+           </li> 
+           <li>
+               <i class="iconfont icon-weixiu"></i>
+               <span>退换修</span>
+           </li> 
+        </ul>
+
+        <yd-cell-group class="mt20">
+            <yd-cell-item arrow>
+                <span slot="left">会员中心</span>
+            </yd-cell-item>
+
+            <yd-cell-item arrow>
+                <span slot="left">我的优惠</span>
+            </yd-cell-item>
+        </yd-cell-group>
+
+        <yd-cell-group>
+            <yd-cell-item arrow>
+                <span slot="left">设置</span>
+            </yd-cell-item>
+        </yd-cell-group>
 
         <transition name="goleft">
             <router-view></router-view>
