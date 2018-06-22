cae7c13eeb096eea7c7c9f26f424b3b93b049b44
Zion0707 <Zion19920707@gmail.com>
2018-06-22 11:42:25 +0800

    change

--- mobile/src/components/my/register.vue
+++ mobile/src/components/my/register.vue
@@ -8,7 +8,45 @@
 		    </yd-navbar>
 	    </div>
 		<div class="cp-body">
-			111
+			
+			<div class="register-logo">
+				<strong>We</strong><span></span>
+			</div>
+
+			<yd-cell-group style="border-top:1px solid #f1f1f1;">
+		        <yd-cell-item>
+		            <span slot="left">用户名：</span>
+		            <yd-input slot="right" v-model.trim="username" placeholder="请输入用户名" max="20"></yd-input>
+		        </yd-cell-item>
+		        <yd-cell-item>
+		            <span slot="left">密码：</span>
+		            <yd-input slot="right" type="password" v-model.trim="password" placeholder="请输入密码"></yd-input>
+		        </yd-cell-item>
+		        <yd-cell-item>
+		            <span slot="left">确认密码：</span>
+		            <yd-input slot="right" type="password" v-model.trim="password2" placeholder="请确认密码"></yd-input>
+		        </yd-cell-item>
+		        <yd-cell-item>
+		            <span slot="left" style="position: relative;">验证码：
+                        <canvas width="100" height="40" class="vCode" id="code"></canvas>
+		            </span>
+		            <yd-input slot="right" v-model.trim="vCode" placeholder="请输入验证码"></yd-input>
+		        </yd-cell-item>
+		    </yd-cell-group>
+			
+
+
+				
+	        <div class="pd1050">
+	        	<yd-button size="large" type="primary" @click.native="upFrom">提交注册</yd-button>
+	        </div>
+
+			<div class="re-miss">
+				<router-link to="/">遇到问题</router-link>
+				<span>|</span>
+				<router-link to="/login">已有账号</router-link>
+			</div>
+
 		</div>
     </div>
 </template>
@@ -17,8 +55,92 @@
 export default {
     data () {
         return {
-
+        	code:'',
+        	username:'',
+        	password:'',
+        	password2:'',
+        	vCode:'',
         }
+    },
+    methods:{
+    	upFrom(){
+    		//表单校验
+    		if (this.username == '') {
+    			this.$dialog.notify({mes: '请输入用户名'});
+    		}else if ( this.password == '' ) {
+    			this.$dialog.notify({mes: '请输入密码'});
+    		}else if ( this.password2 == '' ) {
+    			this.$dialog.notify({mes: '请确认密码'});
+    		}else if ( this.password != this.password2  ) {
+    			this.$dialog.notify({mes: '确认密码不正确'});
+    		}else if ( this.vCode == '' ) {
+                this.$dialog.notify({mes: '请输入验证码'});
+            }else if ( this.vCode != this.code ) {
+                this.$dialog.notify({mes: '验证码不正确'});
+            }else{
+	    		
+	    		console.log(this.username);
+	    		console.log(this.password);
+	    		console.log(this.password2);
+
+    		}
+    	}
+    },
+    mounted(){
+    	var _self = this;
+
+
+        //canvas 验证码生成
+        var cvs = document.getElementById('code');
+        var cxt = cvs.getContext('2d');
+
+
+        function randomNumber(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
+        function randomString() {
+            _self.code = '';
+            var source = '012345789ascdefgqwrtyuioplkjghmnvczx';
+            for (var i = 0; i < 4; i++) {
+                var index = Math.floor(Math.random() * source.length);
+                _self.code = _self.code + source.charAt(index);
+            }
+            return _self.code;
+        }
+        function drawString(code) {
+            cxt.clearRect(0, 0, cvs.width, cvs.height);
+            cxt.fillStyle = '#ccc';
+            cxt.font = '30px 黑体';
+            cxt.textBaseline = 'top';
+            for (var i = 0; i < code.length; i++) {
+                var r = randomNumber(0, 255);
+                var g = randomNumber(0, 255);
+                var b = randomNumber(0, 255);
+                var h = randomNumber(0, cvs.height - 40);
+                cxt.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+                cxt.fillText(code.charAt(i), 10 + i * 18, h);
+            }
+            //线条
+            for (var i = 0; i < 10; i++) {
+                var x1 = randomNumber(0, cvs.width);
+                var y1 = randomNumber(0, cvs.height);
+                var x2 = randomNumber(0, cvs.width);
+                var y2 = randomNumber(0, cvs.height);
+                cxt.strokeStyle = '#ddd';
+                cxt.beginPath();
+                cxt.moveTo(x1, y1);
+                cxt.lineTo(x2, y2);
+                cxt.closePath();
+                cxt.stroke();
+            }
+        }
+
+        drawString(randomString());
+        cvs.onclick = function (e) {
+            drawString(randomString());
+        }
+
+          
     }
 }
 </script>
