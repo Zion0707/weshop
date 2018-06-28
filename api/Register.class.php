728d148fa2b6c1c4c1e5f4957c7f1e01787c6fd4
Zion0707 <Zion19920707@gmail.com>
2018-06-28 16:10:54 +0800

    change

--- api/Register.class.php
+++ api/Register.class.php
@@ -3,61 +3,95 @@
 	require 'Conn.class.php';
 	use Weshop\Api\Conn as Conn;
 
-
-	require 'ShopCar.class.php';
-	use Weshop\Api\ShopCar as ShopCar;
-
 	/**
 	* 注册类
 	*/
 	class Register
 	{ 
+
+		private $pdo;
+		public function __construct()
+		{
+			$Conn = new Conn();
+			$this->pdo = $Conn->connDb();
+		}	
+
+		/*
+		* 判断用户是否已经注册
+		* @return 查询用户的数量
+		*/ 
+		private function isRegister($pdo, $username){
+			$stmt = $pdo->prepare('SELECT `username` FROM user_info WHERE `username`=?');
+			$stmt->bindParam(1, $username);
+			$stmt->execute();
+			$userArr = [];
+			while( $row = $stmt->fetch(PDO::FETCH_ASSOC) ){
+				$userArr[] = $row;
+			}
+			return $userArr;
+		}
+
+		/*
+		* 创建购物车
+		* @param $pdo PDO 对象
+		* @param $uid 用户id
+		*/ 
+		private function createCar($pdo, $uid){
+			$stmt = $pdo->prepare('INSERT INTO shop_car (`uid`, `createTime`) VALUES (?,?) ');
+			$stmt->bindParam(1, $uid);
+			$createTime = time();
+			$stmt->bindParam(2, $createTime);
+			return $stmt->execute();
+		}	
+
+		/*
+		* 注册新用户
+		* @param $pdo PDO 对象
+		*/ 
+		private function registerNewUser($pdo, $username ,$password){
+			//注册新用户
+			$stmt = $pdo->prepare('INSERT INTO user_info (`username`,`password`,`createTime`) VALUES (?,?,?)');			
+			//这里使用用户名和密码作为md5加密，即使两个人密码相同但是用户名不一样，也会生成不一样的密码	
+			$psw = md5( $username.$password );
+			$createTime = time();
+			$stmt->bindParam(1, $username);
+			$stmt->bindParam(2, $psw);
+			$stmt->bindParam(3, $createTime);
+			return $stmt->execute();
+		}
+
+
+
 		/**
 		* @param $post 是注册传递过来的值
 		*/
 		public function registerInput($post){
 			//获取对象参数
 			$postData = json_decode(json_encode($post));
-
 			$username = trim($postData->username);
 			$password = trim($postData->password);
 
 			//判断参数是否正确
 			if ( $username && $password ) {
-				$Conn = new Conn();
-				$pdo = $Conn->connDb();
-				
+				$pdo = $this->pdo;
+
 				//判断用户是否已经注册过
-				$stmt = $pdo->prepare('SELECT `username` FROM user_info WHERE `username`=?');
-				$stmt->bindParam(1, $username);
-				$stmt->execute();
-				$userArr = [];
-				while( $row = $stmt->fetch(PDO::FETCH_ASSOC) ){
-					$userArr[] = $row;
-				}
-				//判断用户数量
+				$userArr = $this->isRegister($pdo, $username);
 				if ( count($userArr) ) {
 					exit( json_encode([
 						'code'=> -2,
 						'msg'=> '该用户名已经注册过！'
 					]) );
 				}
-
-
-				//注册新用户
-				$stmt = $pdo->prepare('INSERT INTO user_info (`username`,`password`,`createTime`) VALUES (?,?,?)');			
-				//这里使用用户名和密码作为md5加密，即使两个人密码相同但是用户名不一样，也会生成不一样的密码	
-				$psw = md5( $username.$password );
-				$createTime = time();
-				$stmt->bindParam(1, $username);
-				$stmt->bindParam(2, $psw);
-				$stmt->bindParam(3, $createTime);
-				if ( $stmt->execute() ) {		
-
+				
+				
+				if ( $this->registerNewUser($pdo, $username, $password) ) {		
+					
 					//根据username查询uid
 					$stmt = $pdo->prepare('SELECT * FROM user_info WHERE `username`=?');
 					$stmt->bindParam(1, $username);
 					$stmt->execute();
+
 					//设置 session 1小时过期时间 和 用户uid
 					if (!session_id()) session_start();
 					$_SESSION['expireTime'] = time() + 3600;
@@ -65,10 +99,8 @@
 						$_SESSION['uid'] = $row['id'];
 					}
 
-
-					//获取uid来进行创建购物车
-					$ShopCar = new ShopCar();
-					$ShopCar->createCar($pdo, $_SESSION['uid']);
+					//创建购物车
+					$this->createCar($pdo, $_SESSION['uid']);
 
 					exit( json_encode([
 						'code'=> 0,
--- api/ShopCar.class.php
+++ api/ShopCar.class.php
@@ -10,15 +10,5 @@
 	*/
 	class ShopCar
 	{
-		/*
-		* 创建购物车
-		* @param $pdo PDO 对象
-		* @param $uid 用户id
-		*/ 
-		public function createCar($pdo, $uid){
-			$stmt = $pdo->prepare('INSERT INTO shop_car (`uid`, `createTime`) VALUES (?,?) ');
-			$stmt->bindParam(1, $uid);
-			$stmt->bindParam(2, time());
-			$stmt->execute();
-		}	
+		
 	}
